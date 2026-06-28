dd60f12a6aa2250b386c24c61af051cbb6389b8c
angeleenavango20 <angeleenavango@gmail.com>
2026-06-28 16:17:52 +0530

    files modified

--- Practice/grade-tracker/tracker/person.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def __init__(self, name: str):
-        self.name = name
-
-    def __str__(self) -> str:
-        return f"Person: {self.name}"
-    
-    def __repr__(self) -> str:
-        return f"Person(name='{self.name}')"
